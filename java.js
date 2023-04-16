1c29984de958a4d2a8dd711b00e2f7fa181bfcff
SanjaykECE <106543778+SanjaykECE@users.noreply.github.com>
2023-04-16 16:19:30 +0530

    Delete java.js

--- java.js
+++
@@ -1,5 +0,0 @@
-const user = localStorage.getItem('username', user_name.value);
- const pass = localStorage.getItem('password', password.value);
- if ( user === user_name.value && password.value === pass ) {
-     location.pathname = "/home/home.html"
- }
